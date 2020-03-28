b2f29dde7cdba830c102872a01e59b5292508449
PRAM0012 <preepram.lea@bru.ac.th>
2020-03-28 18:01:14 +0700

    - FIELD INJECTION

--- app/src/main/java/com/pram/trydagger2/MainActivity.java
+++ app/src/main/java/com/pram/trydagger2/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
-    private Car car;
+    @Inject Car car;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         CarComponent component = DaggerCarComponent.create();
+        component.inject(this);
 
-        car = component.getCar();
         car.drive();
     }
 }
