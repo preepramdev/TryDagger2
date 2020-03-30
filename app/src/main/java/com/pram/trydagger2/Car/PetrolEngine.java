de243d23ce231c814153d452920aa6a004e5cdc4
PRAM0012 <preepram.lea@bru.ac.th>
2020-03-30 12:01:53 +0700

    - @COMPONENT.BUILDER, @BINDSINSTANCE & @NAMED

--- app/src/main/java/com/pram/trydagger2/Car/PetrolEngine.java
+++ app/src/main/java/com/pram/trydagger2/Car/PetrolEngine.java
@@ -3,17 +3,25 @@ package com.pram.trydagger2.Car;
 import android.util.Log;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 public class PetrolEngine implements Engine {
     private static final String TAG = "PetrolEngine";
 
-    @Inject
-    public PetrolEngine() {
+    private int horsePower;
+    private int engineCapacity;
 
+    @Inject
+    public PetrolEngine(@Named("horse power") int horsePower,
+                        @Named("engine capacity") int engineCapacity) {
+        this.horsePower = horsePower;
+        this.engineCapacity = engineCapacity;
     }
 
     @Override
     public void start() {
-        Log.e(TAG, "Petrol engine started");
+        Log.e(TAG, "Petrol engine started. " +
+                "\nHorsepower: " + horsePower +
+                "\nEngine capacity: " + engineCapacity);
     }
 }
--- app/src/main/java/com/pram/trydagger2/dagger/CarComponent.java
+++ app/src/main/java/com/pram/trydagger2/dagger/CarComponent.java
@@ -3,13 +3,28 @@ package com.pram.trydagger2.dagger;
 import com.pram.trydagger2.Car.Car;
 import com.pram.trydagger2.MainActivity;
 
+import javax.inject.Named;
+
+import dagger.BindsInstance;
 import dagger.Component;
 
-//@Component(modules = {WheelsModule.class, PetrolEngineModule.class})
-@Component(modules = {WheelsModule.class, DieselEngineModule.class})
+@Component(modules = {WheelsModule.class, PetrolEngineModule.class})
+//@Component(modules = {WheelsModule.class, DieselEngineModule.class})
 public interface CarComponent {
 
     Car getCar();
 
     void inject(MainActivity mainActivity);
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        Builder horsePower(@Named("horse power") int horsePower);
+
+        @BindsInstance
+        Builder engineCapacity(@Named("engine capacity") int engineCapacity);
+
+        CarComponent build();
+    }
 }
